31dc5e337c438b159b1f168d1a5823db17a639e7
xyhlgf <13288276369@163.com>
2025-07-13 03:39:35 +0800

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -24,3 +24,26 @@ const hookClick = (e) => {
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('<div class="navbar-brand"><a class="navbar-logo" href="https://hlgf001.top/index"><img src="https://hlgf001.top/index/wp-content/uploads/2025/07/扫光logo.gif" switch-src="https://hlgf001.top/index/wp-content/uploads/2025/07/扫光logo蓝色.gif" alt="欢乐工坊官方网站">
+			</a></div>');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
